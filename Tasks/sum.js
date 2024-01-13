ef43224ebbab8beca4bf6745034927708513c6b6
invia <inviakonstantin@gmail.com>
2024-01-14 01:39:09 +0200

    Removed unnecessary line, changed type of variable sum(from array to number)

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,13 +1,12 @@
 // Sum all numbers from an array
 
 sum = (array) => {
-  sum = [0];
-  let k = 5;
-  for (i of array) {
-    let t = typeof i;
+  sum = 0;
+  for (index of array) {
+    let t = typeof index;
     if (t === 'number') {
       if (sum.length > 0) {
-        const new_Sum = sum[sum.length - 1] + i;
+        const new_Sum = sum[sum.length - 1] + index;
         sum.push(new_Sum);
       }
     }
