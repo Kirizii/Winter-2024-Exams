18e465fe1d2965235cdad91bce5c6aead24c0347
Kirizii <143125175+Kirizii@users.noreply.github.com>
2024-01-13 18:06:09 +0200

    Changed naming(days to DAYS)

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,11 +1,11 @@
 // Get day number
 "use strict"
 
-const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDay = (day) => {
-  for (let index = 0; index < days.length; index++) {
-    if (day.startsWith(days[index])) {
+  for (let index = 0; index < DAYS.length; index++) {
+    if (day.startsWith(DAYS[index])) {
       return index + 1;
     }
   }
