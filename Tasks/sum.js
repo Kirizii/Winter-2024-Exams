de6d3c81e39f2340ad93c81f2e32d61b24d24c23
invia <inviakonstantin@gmail.com>
2024-01-14 22:22:44 +0200

    (sum.js update)Added semicolon, added const instead of let

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,11 +1,11 @@
 // Sum all numbers from an array
 // Test sum.js: Passed: 7 of 7
 
-"use strict"
+"use strict";
 
 const sum = (array) => {
   let result = 0;
-  for (let value of array) {
+  for (const value of array) {
     if (typeof value === 'number') {
       result += value;
     }
