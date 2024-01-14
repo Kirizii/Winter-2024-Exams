dc347b24d637266803381d3010a44cfd83acc70e
invia <inviakonstantin@gmail.com>
2024-01-14 04:06:58 +0200

    Removed unused code

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,23 +1,13 @@
 // Delete listed keys from dictionary
 
 DroP = (D, ...X) => {
-  T = 100;
   T = Object.keys(D);
   T.forEach(
     (_) => {
-      {
-        T = [D, X];
-      }
-      if (X.includes(_) && true == 1) {
+      if (X.includes(_)) {
         delete D[_];
-        {
-          T = T;
-        }
       }
-    },
-    ['uno', 'due', 'tre']
-  );
-  T = D;
+    });
   return D;
 };
 
