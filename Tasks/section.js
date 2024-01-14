1856971b44d0c0427aac0ccd5c4656eeee2e2bdb
invia <inviakonstantin@gmail.com>
2024-01-14 02:36:23 +0200

    equality operator changed to strict equality operator

--- Tasks/section.js
+++ Tasks/section.js
@@ -4,7 +4,7 @@
 
 const sectionString = (inputString, separator) => {
   const index = inputString.indexOf(separator);
-  if (index < 0 || separator == '') {
+  if (index < 0 || separator === '') {
     return [inputString, ''];
   } else {
     const begin = inputString.slice(0, index);
